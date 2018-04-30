aa9f080705caa5f5b07dc9f43d727ebbb5c40d6f
Alek <alek2750@gmail.com>
2018-04-30 10:36:26 +0200

    True fix til SQL

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -1,86 +1,98 @@
-CREATE DATABASE  IF NOT EXISTS `useradmin`;
-
-
-USE `useradmin`;
-
-DROP TABLE IF EXISTS `Users`;
-DROP TABLE IF EXISTS `Orders`;
-DROP TABLE IF EXISTS `Carport`;
-DROP TABLE IF EXISTS `Materials`;
-DROP TABLE IF EXISTS `Order_has_Materials`;
-
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema useradmin
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Table `mydb`.`Users`
+-- Schema useradmin
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE SCHEMA IF NOT EXISTS `useradmin` DEFAULT CHARACTER SET latin1 ;
+USE `useradmin` ;
+
+DROP TABLE IF EXISTS `Orders_has_Materials`;
+DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `Orders`;
+DROP TABLE IF EXISTS `Materials`;
+
+-- -----------------------------------------------------
+-- Table `useradmin`.`Materials`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `useradmin`.`Materials` (
+  `MaterialsID` INT(11) NOT NULL,
+  `MaterialsName` VARCHAR(45) NOT NULL,
+  `QuantityInOrder` INT(11) NOT NULL,
+  `MaterialsInStock` INT(11) NOT NULL,
+  `MaterialsInOrders` INT(11) NOT NULL,
+  PRIMARY KEY (`MaterialsID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `useradmin`.`Users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `useradmin`.`Users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
-  `role` varchar(20) NOT NULL DEFAULT 'customer',
+  `role` VARCHAR(20) NOT NULL DEFAULT 'customer',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Order`
+-- Table `useradmin`.`Orders`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Orders` (
-  `idOrder` INT NOT NULL ,
-  `OrderDate` INT NOT NULL,
-  `PriceTotal` INT NOT NULL,
-  `Users_id` INT NOT NULL,
-	`Address` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `useradmin`.`Orders` (
+  `idOrder` INT(11) NOT NULL,
+  `OrderDate` INT(11) NOT NULL,
+  `PriceTotal` INT(11) NOT NULL,
+  `Address` VARCHAR(45) NOT NULL,
   `Phone` VARCHAR(45) NOT NULL,
+  `Users_id` INT(11) NOT NULL,
   PRIMARY KEY (`idOrder`),
-  INDEX `fk_Orders_Users_idx` (`Users_id` ASC),
-  CONSTRAINT `fk_Orders_Users`
+  INDEX `fk_Orders_Users1_idx` (`Users_id` ASC),
+  CONSTRAINT `fk_Orders_Users1`
     FOREIGN KEY (`Users_id`)
-    REFERENCES `Users` (`id`)
+    REFERENCES `useradmin`.`Users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `mydb`.`Materials`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Materials` (
-  `MaterialsID` INT NOT NULL,
-  `MaterialsName` VARCHAR(45) NOT NULL,
-  `QuantityInOrder` INT NOT NULL,
-  `MaterialsInStock` INT NOT NULL,
-  `MaterialsInOrders` INT NOT NULL,
-  PRIMARY KEY (`MaterialsID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Order_has_Materials`
+-- Table `useradmin`.`Orders_has_Materials`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Orders_has_Materials` (
-  `Orders_idOrder` INT NOT NULL,
-  `Materials_MaterialsID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `useradmin`.`Orders_has_Materials` (
+  `Orders_idOrder` INT(11) NOT NULL,
+  `Materials_MaterialsID` INT(11) NOT NULL,
   PRIMARY KEY (`Orders_idOrder`, `Materials_MaterialsID`),
   INDEX `fk_Orders_has_Materials_Materials1_idx` (`Materials_MaterialsID` ASC),
-  INDEX `fk_Orders_has_Materials_Order1_idx` (`Orders_idOrder` ASC),
-  CONSTRAINT `fk_Orders_has_Materials_Order1`
+  INDEX `fk_Orders_has_Materials_Orders1_idx` (`Orders_idOrder` ASC),
+  CONSTRAINT `fk_Orders_has_Materials_Orders1`
     FOREIGN KEY (`Orders_idOrder`)
-    REFERENCES `Orders` (`idOrder`)
+    REFERENCES `useradmin`.`Orders` (`idOrder`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orders_has_Materials_Materials1`
     FOREIGN KEY (`Materials_MaterialsID`)
-    REFERENCES `Materials` (`MaterialsID`)
+    REFERENCES `useradmin`.`Materials` (`MaterialsID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
@@ -95,5 +107,7 @@ INSERT INTO `Users` VALUES
 (3,'robin@somewhere.com','batman','employee');
 LOCK TABLES `Orders` WRITE;
 INSERT INTO `Orders` VALUES
-(1, 24-04-2018, 8500, 1,'hjortevej 9','88888888');
+(1, 24-04-2018, 8500, 1,'hjortevej 9','88888888'),
+(2, 24-04-2018, 1500, 10,'hjortevej 29','88888888'),
+(3, 24-04-2018, 9001, 69,'hjortevej 69','88888888');
 UNLOCK TABLES;
