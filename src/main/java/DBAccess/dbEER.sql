2222e108fd597acd137fc40cbe93af5028ad5e44
Alek <alek2750@gmail.com>
2018-05-11 11:41:22 +0200

    Finale DB?

--- src/main/java/DBAccess/dbEER.sql
+++ src/main/java/DBAccess/dbEER.sql
@@ -10,25 +10,36 @@ DROP TABLE IF EXISTS `Users`;
 DROP TABLE IF EXISTS `Orders`;
 DROP TABLE IF EXISTS `Materials`;
 DROP TABLE IF EXISTS `Carport`;
+DROP TABLE IF EXISTS `Type_Materials`;
 
 -- -----------------------------------------------------
+-- MySQL Workbench Forward Engineering
+
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema useradmin
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `useradmin` DEFAULT CHARACTER SET latin1 ;
+USE `useradmin` ;
 
 -- -----------------------------------------------------
 -- Table `useradmin`.`Carport`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `useradmin`.`Carport` (
-  `idCarport` INT NOT NULL AUTO_INCREMENT,
-  `type_skur` TINYINT NOT NULL,
-  `tag_fladt` TINYINT NOT NULL,
-  `tag_m_rejsning` TINYINT NOT NULL,
+  `idCarport` INT(11) NOT NULL AUTO_INCREMENT,
+  `type_skur` TINYINT(4) NOT NULL,
+  `tag_fladt` TINYINT(4) NOT NULL,
+  `tag_m_rejsning` TINYINT(4) NOT NULL,
   PRIMARY KEY (`idCarport`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = latin1;
+
 
 -- -----------------------------------------------------
 -- Table `useradmin`.`Materials`
@@ -38,6 +49,7 @@ CREATE TABLE IF NOT EXISTS `useradmin`.`Materials` (
   `MaterialsName` VARCHAR(45) NOT NULL,
   `MaterialsInStock` INT(11) NOT NULL,
   `Pris` INT(11) NOT NULL,
+  `Type` INT NOT NULL,
   PRIMARY KEY (`MaterialsID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
@@ -67,18 +79,18 @@ CREATE TABLE IF NOT EXISTS `useradmin`.`Orders` (
   `length` INT(11) NOT NULL,
   `height` INT(11) NOT NULL DEFAULT '250',
   `Users_id` INT(11) NOT NULL,
-  `Carport_idCarport` INT NOT NULL,
+  `Carport_idCarport` INT(11) NOT NULL,
   PRIMARY KEY (`idOrder`),
   INDEX `fk_Orders_Users1_idx` (`Users_id` ASC),
   INDEX `fk_Orders_Carport1_idx` (`Carport_idCarport` ASC),
-  CONSTRAINT `fk_Orders_Users1`
-    FOREIGN KEY (`Users_id`)
-    REFERENCES `useradmin`.`Users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orders_Carport1`
     FOREIGN KEY (`Carport_idCarport`)
     REFERENCES `useradmin`.`Carport` (`idCarport`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Orders_Users1`
+    FOREIGN KEY (`Users_id`)
+    REFERENCES `useradmin`.`Users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -92,17 +104,35 @@ DEFAULT CHARACTER SET = latin1;
 CREATE TABLE IF NOT EXISTS `useradmin`.`Line_item` (
   `Orders_idOrder` INT(11) NOT NULL,
   `Materials_MaterialsID` INT(11) NOT NULL,
-  `Antal` INT(11) NOT NULL,
-  `Pris` INT(11) NOT NULL,
+  `antal` INT(11) NOT NULL,
+  `pris` INT(11) NOT NULL,
   PRIMARY KEY (`Orders_idOrder`, `Materials_MaterialsID`),
   INDEX `fk_Orders_has_Materials_Materials1_idx` (`Materials_MaterialsID` ASC),
   INDEX `fk_Orders_has_Materials_Orders_idx` (`Orders_idOrder` ASC),
+  CONSTRAINT `fk_Orders_has_Materials_Materials1`
+    FOREIGN KEY (`Materials_MaterialsID`)
+    REFERENCES `useradmin`.`Materials` (`MaterialsID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orders_has_Materials_Orders`
     FOREIGN KEY (`Orders_idOrder`)
     REFERENCES `useradmin`.`Orders` (`idOrder`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Orders_has_Materials_Materials1`
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `useradmin`.`Type_Materials`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `useradmin`.`Type_Materials` (
+  `idType_Materials` INT(11) NOT NULL AUTO_INCREMENT,
+  `Product` VARCHAR(45) NULL DEFAULT NULL,
+  `Materials_MaterialsID` INT(11) NOT NULL,
+  PRIMARY KEY (`idType_Materials`),
+  INDEX `fk_Type_Materials_Materials1_idx` (`Materials_MaterialsID` ASC),
+  CONSTRAINT `fk_Type_Materials_Materials1`
     FOREIGN KEY (`Materials_MaterialsID`)
     REFERENCES `useradmin`.`Materials` (`MaterialsID`)
     ON DELETE NO ACTION
