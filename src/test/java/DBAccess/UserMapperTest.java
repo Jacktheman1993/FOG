43d7e1a68d1953cf69673fd73558ec008b97cbcd
Alek <alek2750@gmail.com>
2018-05-17 15:50:17 +0200

    Test(Junit) works again

--- src/test/java/DBAccess/UserMapperTest.java
+++ src/test/java/DBAccess/UserMapperTest.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- *
+ */
 package DBAccess;
 
 import FunctionLayer.LoginSampleException;
@@ -34,16 +34,16 @@ public class UserMapperTest {
     @Before
     public void setUp() {
         try {
-            // awoid making a new connection for each test
+             //awoid making a new connection for each test
             if ( testConnection == null ) {
                 String url = String.format( "jdbc:mysql://%s:3306/%s", HOST, DBNAME );
                 Class.forName( "com.mysql.jdbc.Driver" );
 
                 testConnection = DriverManager.getConnection( url, USER, USERPW );
-                // Make mappers use test 
+               //  Make mappers use test 
                 Connector.setConnection( testConnection );
             }
-            // reset test database
+             //reset test database
             try ( Statement stmt = testConnection.createStatement() ) {
                 stmt.execute( "drop table if exists Users" );
                 stmt.execute( "create table Users like UsersTest" );
@@ -58,38 +58,37 @@ public class UserMapperTest {
 
     @Test
     public void testSetUpOK() {
-        // Just check that we have a connection.
+         //Just check that we have a connection.
         assertNotNull( testConnection );
     }
 
     @Test
     public void testLogin01() throws LoginSampleException {
-        // Can we log in
+         //Can we log in
         User user = UserMapper.login( "jens@somewhere.com", "jensen" );
         assertTrue( user != null );
     }
 
     @Test( expected = LoginSampleException.class )
     public void testLogin02() throws LoginSampleException {
-        // We should get an exception if we use the wrong password
+         //We should get an exception if we use the wrong password
         User user = UserMapper.login( "jens@somewhere.com", "larsen" );
     }
 
     @Test
     public void testLogin03() throws LoginSampleException {
-        // Jens is supposed to be a customer
+         //Jens is supposed to be a customer
         User user = UserMapper.login( "jens@somewhere.com", "jensen" );
         assertEquals( "customer", user.getRole() );
     }
 
     @Test
     public void testCreateUser01() throws LoginSampleException {
-        // Can we create a new user - Notice, if login fails, this will fail
-        // but so would login01, so this is OK
+         //Can we create a new user - Notice, if login fails, this will fail
+         //but so would login01, so this is OK
         User original = new User( "king@kong.com", "uhahvorhemmeligt", "konge" );
         UserMapper.createUser( original );
         User retrieved = UserMapper.login( "king@kong.com", "uhahvorhemmeligt" );
         assertEquals( "konge", retrieved.getRole() );
     }
 }
-*/
